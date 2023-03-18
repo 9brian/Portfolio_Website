9339d266e5053a93e58098542ae35a3c01a744ca
Brian Palomar <104478722+9brian@users.noreply.github.com>
2023-03-18 12:46:29 -0700

    Debuggin

--- scripts/first.js
+++ scripts/first.js
@@ -1,5 +1,6 @@
 document.getElementById("initialButton").addEventListener("click", redirectToHome);
 function redirectToHome() {
   window.location.href = 'index.html';
+  console.log("working")
 };
 
